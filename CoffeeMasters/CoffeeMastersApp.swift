1378cdb023f465ab88bf29ea81c083a7d0837c5f
AhmedEid3 <ahmed.eid3@outlook.com>
2023-07-08 11:15:38 +0300

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Ahmed Eid on 08/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
